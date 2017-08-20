cafb69ef48c14d1aac9df410d7b5c1ccd970ed89
Francis Murillo <francisavmurillo@gmail.com>
2017-08-20 15:22:27 +0800

    Commit data extraction

--- server/server.js
+++ server/server.js
@@ -6,27 +6,30 @@
 /* eslint no-console: [0]*/
 /* eslint no-sync: [0]*/
 import bodyParser from "body-parser";
-import commandExists from "command-exists";
 import dotenv from "dotenv";
 import express from "express";
 import fs from "fs";
+import path from "path";
+import shell from "shelljs";
 
-dotenv.config({"path": "../.env"});
+dotenv.config();
 
 const {
     "JAVA_COMMAND": javaCommand,
     "GIT_COMMAND": gitCommand,
     "CODE_MAAT_JAR_FILE": codeMaatJarFile,
-    "PROJECT_DIR": projectDir
+    "PROJECT_DIR": baseProjectDir
 } = process.env;
 
-if (!commandExists.sync(javaCommand)) {
+const projectDir = path.resolve(baseProjectDir);
+
+if (!shell.which(javaCommand)) {
     console.error(`Java command(JAVA_COMMAND) does not exist:
  ${javaCommand}`);
     process.exit(1);
 }
 
-if (!commandExists.sync(gitCommand)) {
+if (!shell.which(gitCommand)) {
     console.error(`Git command(GIT_COMMAND) does not exist:
  ${gitCommand}`);
     process.exit(1);
@@ -44,9 +47,37 @@ if (!fs.existsSync(projectDir)) {
     process.exit(1);
 }
 
-console.log("Using these settings:");
-console.log(process.env);
+console.log("Read the configuration file .env");
+console.log(`Project directory: ${projectDir}`);
 
+const logDirName = ".logs";
+const logDir = path.join(projectDir, logDirName);
+shell.mkdir("-p", logDir);
+console.log("Created log directory");
+
+const commitFileName = ".commits";
+const commitFile = path.join(logDir, commitFileName);
+
+let commitData = "";
+shell.exec(
+    `${gitCommand} log --format=%cI,%H`,
+    {"cwd": projectDir},
+    (code, stdout, _stderr) => {
+        if (code > 0) {
+            console.error("Could not get commit data");
+            process.exit(1);
+        } else {
+            commitData = stdout
+                .split("\n")
+                .map((line) => line.split(","))
+                .map(([date, hash]) => ({
+                    date,
+                    hash
+                }));
+        }
+    });
+
+console.log("Acquired commit data");
 
 const app = express();
 app.use(bodyParser.json());
@@ -84,6 +115,10 @@ router.get("/code-maat", (req, res) => {
     } else if (!isDateValue(endDate)) {
         res.status(422)
             .send(`End Date(end_date) is a required date: ${endDate}`);
+    } else if (endDate < startDate) {
+        res.status(422)
+            .send(`Start Date is greater than End Date:
+ ${startDate} > ${endDate}`);
     } else if (!isAnalysisValue(analysis)) {
         res.status(422)
             .send(`Analysis(analysis) is not a valid analysis type:
