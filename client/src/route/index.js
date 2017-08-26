076278ea4061ae88b6189fbcaa9ed9f02d1e4372
Francis Murillo <francisavmurillo@gmail.com>
2017-08-26 19:27:44 +0800

    Added routing

--- client/src/route/index.js
+++ client/src/route/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {div} from "react-dom";
+import {h1} from "react-dom";
 import {Route} from "react-router";
 
 export default () => (
@@ -7,12 +7,15 @@ export default () => (
         <Route
             exact
             path={"/"}
+            component={() => (<h1>{"AX"}</h1>)}
         />
         <Route
             path={"/summary"}
+            component={() => (<h1>{"BET"}</h1>)}
         />
         <Route
             path={"/revision"}
+            component={() => (<h1>{"MEOW"}</h1>)}
         />
         <Route
             path={"/coupling"}
