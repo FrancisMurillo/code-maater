3770cc701c31d8f651b46df94d07beee655fe477
Francis Murillo <francisavmurillo@gmail.com>
2017-08-28 13:10:41 +0800

    Working absolute churn

--- client/src/route/index.js
+++ client/src/route/index.js
@@ -5,6 +5,7 @@ import SummaryScreen from "../summary";
 import RevisionScreen from "../revision";
 import CouplingScreen from "../coupling";
 import AgeScreen from "../age";
+import AbsoluteChurnScreen from "../absoluteChurn";
 
 export default () => (
     <div>
@@ -30,6 +31,7 @@ export default () => (
         />
         <Route
             path={"/absoluteChurn"}
+            component={AbsoluteChurnScreen}
         />
         <Route
             path={"/authorChurn"}
